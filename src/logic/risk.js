42a51aec9c195ca9cf2dd0b9c7760b23bc054b59
Steffan Halvorsen <steffanhalv@gmail.com>
2017-07-29 17:44:52 +0200

    Added game logic folder

---
+++ src/logic/risk.js
@@ -0,0 +1,5 @@
+Risk.install = function (Vue) {
+  Vue.prototype.$risk = () => {
+    console.log('works!')
+  }
+}
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Risk from '../logic/risk'
 
 import App from './App'
 import router from './router'
@@ -7,6 +8,7 @@ import router from './router'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.use(Risk)
 
 /* eslint-disable no-new */
 new Vue({
