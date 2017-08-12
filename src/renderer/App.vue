63041714e6426c8444108312de20d2ebf8e5a278
Steffan Halvorsen <steffanhalv@gmail.com>
2017-08-12 02:27:46 +0200

    centered map

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -12,4 +12,15 @@
 
 <style>
   /* CSS */
+  body {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    margin: 0;
+  }
+  #app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
 </style>
